f2f529e49cf035fbac71ad73a7279bf90d7c8403
Guillaume Tardif <guillaume.tardif@docker.com>
2017-09-05 18:54:27 +0200

    Include hub PR creation

--- run.sh
+++ run.sh
@@ -20,11 +20,8 @@ git checkout -b release_notes_${VERSION}
 cd ..
 
 wget https://download.docker.com/${ARCH}/${CHANNEL}/${BUILD_NUMBER}/NOTES
-cat NOTES
-
 #ad version header
 sed -i '1s/^/\n'"### Docker Community Edition ${VERSION} Release Notes (2017-08-31) (${CAPITALIZED_CHANNEL})"'\n\n/' NOTES
-cat NOTES
 
 #update docs
 sed -i -e '/'"## ${CAPITALIZED_CHANNEL} Release Notes"'/r NOTES' "./sources/docker-for-${ARCH_LABEL}/release-notes.md"
@@ -32,8 +29,6 @@ sed -i -e '/'"## ${CAPITALIZED_CHANNEL} Release Notes"'/r NOTES' "./sources/dock
 grep -C 10 "## Edge Release Notes" ./sources/docker-for-windows/release-notes.md
 
 cd sources
-git status
-git diff
 
 git config --global user.name "${USER_NAME}"
 git config --global user.email "${USER_EMAIL}"
@@ -41,5 +36,4 @@ git config --global user.email "${USER_EMAIL}"
 git commit -asm "Docker for ${ARCH_LABEL} ${CHANNEL} relnotes ${VERSION}"
 
 git push origin release_notes_${VERSION}
-#hub pull-request -m "Test automated release notes PR" -b "docker/docker.github.io.git:master"
-#grep -C 10 "## Edge Release Notes" ./sources/docker-for-windows/release-notes.md
+hub pull-request -m "Test automated release notes PR for ${VERSION}" -b "docker/docker.github.io.git:master"

afd4bc5567975f5e9def617683ea3baf088b269a
Guillaume Tardif <guillaume.tardif@docker.com>
2017-09-07 09:25:20 +0200

    Fixing date in release notes
    Signed-off-by: Guillaume Tardif <guillaume.tardif@docker.com>

--- run.sh
+++ run.sh
@@ -9,6 +9,17 @@ else
 	ARCH_LABEL="windows"
 fi
 
+TODAY=$(date +'%Y-%m-%d')
+
+wget https://download.docker.com/${ARCH}/${CHANNEL}/${BUILD_NUMBER}/NOTES
+# delete all leading blank lines at top of file
+sed -i '/./,$!d' NOTES
+# delete all trailing blank lines at end of file
+sed -i -e :a -e '/^\n*$/{$d;N;ba' -e '}'  NOTES
+#ad version header
+sed -i '1s/^/\n'"### Docker Community Edition ${VERSION} ${TODAY} (${CAPITALIZED_CHANNEL})"'\n\n/' NOTES
+cat NOTES
+
 echo "Get sources"
 git clone https://${GITHUB_TOKEN}@github.com/pinata-mac/docker.github.io.git sources
 cd sources 
@@ -18,20 +29,10 @@ git rebase upstream/master
 git push origin master
 git checkout -b release_notes_${VERSION}
 cd ..
-
-wget https://download.docker.com/${ARCH}/${CHANNEL}/${BUILD_NUMBER}/NOTES
-# delete all leading blank lines at top of file
-sed -i '/./,$!d' NOTES
-# delete all trailing blank lines at end of file
-sed -i -e :a -e '/^\n*$/{$d;N;ba' -e '}'  NOTES
-#ad version header
-sed -i '1s/^/\n'"### Docker Community Edition ${VERSION} 2017-08-31 (${CAPITALIZED_CHANNEL})"'\n\n/' NOTES
-cat NOTES
-
 #update docs
 sed -i -e '/'"## ${CAPITALIZED_CHANNEL} Release Notes"'/r NOTES' "./sources/docker-for-${ARCH_LABEL}/release-notes.md"
 
-grep -C 10 "## Edge Release Notes" ./sources/docker-for-windows/release-notes.md
+grep -C 10 "## ${CAPITALIZED_CHANNEL} Release Notes" ./sources/docker-for-${ARCH_LABEL}/release-notes.md
 
 cd sources
 git config --global user.name "${USER_NAME}"
